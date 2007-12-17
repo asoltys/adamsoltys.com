676b22f9232e64761e72742853630ec6cf916d70
Adam <Adam@afe20696-ce0c-a340-9be7-82da27170024>
2007-12-17 07:59:06 +0000

    Trying to serve HTML to non-XHTML compliant browsers
    git-svn-id: svn://adamsoltys.com/adam_soltys@22 afe20696-ce0c-a340-9be7-82da27170024

--- trunk/app/controllers/application.rb
+++ trunk/app/controllers/application.rb
@@ -3,8 +3,47 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
+	before_filter :adjust_format_for_non_xhtml_browsers
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'b94e9d30ec6c1e0abeed01fd8387a177'
+	
+	def home
+		respond_to do |format|
+			format.html
+			format.xhtml
+		end
+	end
+	
+	def about
+		respond_to do |format|
+			format.html
+			format.xhtml
+		end
+	end
+	
+	def contact
+		respond_to do |format|
+			format.html
+			format.xhtml
+		end
+	end
+	
+	def resume
+		respond_to do |format|
+			format.html
+			format.xhtml
+		end
+	end
+	
+	private
+
+  def adjust_format_for_non_xhtml_browsers    
+    request.format = :html if non_xhtml_browser?
+  end
+	
+	def non_xhtml_browser?
+    return (request.env["HTTP_ACCEPT"].split(',').include?("application/xhtml+xml")
+  end
 end
