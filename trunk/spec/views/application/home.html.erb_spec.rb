cec79977c787b7f687dbc10a96b7190cc240f470
Adam <Adam@afe20696-ce0c-a340-9be7-82da27170024>
2008-02-05 07:05:18 +0000

    Minor tweaks, improved hCard, added files folder
    git-svn-id: svn://adamsoltys.com/adam_soltys@35 afe20696-ce0c-a340-9be7-82da27170024

--- trunk/spec/views/application/home.html.erb_spec.rb
+++ trunk/spec/views/application/home.html.erb_spec.rb
@@ -1,7 +1,8 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "home page" do
-	it "should display the title of the application" do
-		response.should have_tag('a', 'Adam Soltys')
+	it "should have a link to the contact page for suggestions" do
+		render "/application/home.erb"
+		response.should have_tag('a', 'let me know')
 	end
 end
