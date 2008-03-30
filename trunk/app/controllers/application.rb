e5d3097d72e2be13d1695476f0b4b71ff3a9f944
Adam <Adam@afe20696-ce0c-a340-9be7-82da27170024>
2008-03-30 04:34:08 +0000

    Added caching
    git-svn-id: svn://adamsoltys.com/adam_soltys@44 afe20696-ce0c-a340-9be7-82da27170024

--- trunk/app/controllers/application.rb
+++ trunk/app/controllers/application.rb
@@ -4,18 +4,29 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 	before_filter :adjust_request_format
-	caches_page :lists
+	caches_page :home, :lists, :resume, :contact
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '3f8438829100c0cf5f5a5ed73c0ba3792bd5dd1c0988b01bbe28998d2b21d8fdc0fd77d0a227d236443ee1307647cfd935b81524ba054c79cd381ac65467d370'
 	
-	before_filter :adjust_request_format
+	def home
+		respond_appropriately
+	end
 	
 	def lists
 		@transactions = Transaction.find(:all)
+		respond_appropriately
 	end
-
+	
+	def resume
+		respond_appropriately
+	end
+	
+	def contact
+		respond_appropriately
+	end
+	
 	private
 	
 	def respond_appropriately	
--- trunk/app/controllers/projects_controller.rb
+++ trunk/app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+	caches_page :index
+	
   def index
 		respond_appropriately
   end
