607d2096842e4c75b677cc5ddb1b21bd6cc122d8
adam <adam@afe20696-ce0c-a340-9be7-82da27170024>
2008-04-05 19:45:00 +0000

    Updated mongrel config for linux server
    git-svn-id: svn://adamsoltys.com/adam_soltys@51 afe20696-ce0c-a340-9be7-82da27170024

--- trunk/config/environment.rb
+++ trunk/config/environment.rb
@@ -9,7 +9,7 @@ RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-require 'ROXML'
+require 'roxml'
 
 Rails::Initializer.run do |config|	
 	config.action_controller.page_cache_directory = RAILS_ROOT + "/public/cache/"
