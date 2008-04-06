5a50d00e8088f9830b52e6e92564c534850d5d93
adam <adam@afe20696-ce0c-a340-9be7-82da27170024>
2008-04-06 06:16:48 +0000

    Change deployment settings
    git-svn-id: svn://adamsoltys.com/adam_soltys@56 afe20696-ce0c-a340-9be7-82da27170024

--- trunk/config/deploy.rb
+++ trunk/config/deploy.rb
@@ -1,12 +1,14 @@
-ssh_options[:port] = 443
-set :password, "MPJzfq97a"
+set :username, 'adam'
+set :password, "MPJzfq97"
 set :application, "adam_soltys"
 set :repository,  "svn://adamsoltys.com/adam_soltys/trunk"
+
+ssh_options[:username] = 'adam'
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "C:/Inetpub/wwwroot/#{application}"
+set :deploy_to, "/var/www/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
@@ -16,6 +18,18 @@ role :app, "www.adamsoltys.com"
 role :web, "www.adamsoltys.com"
 role :db,  "www.adamsoltys.com", :primary => true
 
-task :spinner, :roles => :app do
-  run "mongrel_rails start -e production -p 3005 -d -c #{current_path}"
+namespace :deploy do
+	desc <<-DESC
+    Start the application
+  DESC
+	task :start, :roles => :app do
+		run "ln -fs #{current_path}/config/mongrel_cluster.yml /etc/mongrel_cluster/#{application}.yml && /etc/init.d/mongrel_cluster start"
+	end
+	
+	desc <<-DESC
+    Stop the application
+  DESC
+	task :stop, :roles => :app do
+		run "/etc/init.d/mongrel_cluster stop"
+	end
 end
