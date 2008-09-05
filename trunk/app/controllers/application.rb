be876970f28baf5ad24a432bdd20a63fd1b50c84
adam <adam@afe20696-ce0c-a340-9be7-82da27170024>
2008-09-05 07:12:31 +0000

    Posts ain't quite ready for primetime
    git-svn-id: svn://adamsoltys.com/adam_soltys@107 afe20696-ce0c-a340-9be7-82da27170024

--- trunk/app/controllers/application.rb
+++ trunk/app/controllers/application.rb
@@ -13,7 +13,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # :secret => '3f8438829100c0cf5f5a5ed73c0ba3792bd5dd1c0988b01bbe28998d2b21d8fdc0fd77d0a227d236443ee1307647cfd935b81524ba054c79cd381ac65467d370'
 	
 	def home
-		@post = Post.find(:last)
 		respond_appropriately
 	end
 	
