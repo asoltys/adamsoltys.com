318c266fda1005f7b8ae21eba725f1ee94aa7861
Adam Soltys <asoltys@gmail.com>
2008-12-02 20:31:48 -0800

    Removing unused plugins

--- trunk/app/models/transaction.rb
+++
@@ -1,9 +0,0 @@
-class Transaction < ActiveRecord::Base
-	require 'htmlentities'
-	belongs_to :account
-	
-	def html_encoded_name
-		coder = HTMLEntities.new
-		coder.encode(name, :named)
-	end
-end
--- trunk/vendor/plugins/awesomeness/lib/awesomeness/core_ext/object.rb
+++
@@ -1,8 +0,0 @@
-class Object
-  # See http://moonbase.rydia.net/mental/blog/programming/eavesdropping-on-expressions
-  # Will be added in Ruby 1.9
-  def tap
-    yield self
-    self
-  end if RUBY_VERSION < '1.9'
-end
