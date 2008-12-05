b7f73004f20910f1f8a0f56ce124a032285c22c3
Adam Soltys <asoltys@gmail.com>
2008-12-04 21:18:38 -0800

    Finished rewriting lists javascript in jQuery

--- public/javascripts/lists.js
+++ public/javascripts/lists.js
@@ -1,9 +1,7 @@
-var SIZE = 3;
-
 $(function() {
   var more_link = "<a class=\"more\">More...</a>";
-  $('div#content ul').filter(needShrinking).after(more_link).each(collapse);
-  $('a.more').click(expandList);
+  $('div#content ul').filter(needShrinking).after(more_link).find("li:gt(3)").hide();
+  $('a.more').click(toggleList);
 });
 
 function needShrinking()
@@ -11,18 +9,8 @@ function needShrinking()
   return $(this).children().length > 3;
 }
 
-function expandList()
+function toggleList()
 {
-  var list = $(this).previous();
-  list.expand();
-}
-
-function expand()
-{
-  $(this).find("li:gt(3)").show();
-}
-
-function collapse()
-{
-  $(this).find("li:gt(3)").hide();
+  $(this).prev().find("li:gt(3)").toggle();
+  $(this).text() == 'More...' ? $(this).text('Less...') : $(this).text('More...');
 }
