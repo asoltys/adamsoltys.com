e1df51ed4169760ef0e6d2092f1a8cb6c4dffb0c
Adam Soltys <adam.soltys@pwgsc.gc.ca>
2009-01-22 14:57:25 -0800

    Got rid of signup page

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,6 @@ ActionController::Routing::Routes.draw do |map|
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
 	
 	# Finance Routes
 	
