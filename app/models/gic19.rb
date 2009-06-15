792dd67817d2cef9673ac428dbdcbbe253b58a09
Adam Soltys <asoltys@gmail.com>
2009-06-14 21:09:37 -0700

    Cleaning up specs

--- app/models/gic19.rb
+++
@@ -1,2 +0,0 @@
-class GIC19 < Account
-end
--- spec/views/layouts/_navigation.erb_spec.rb
+++ spec/views/layouts/_navigation.erb_spec.rb
@@ -4,10 +4,10 @@ describe "navigation" do
 	it "should have a link to each of the main pages of the site" do
 		render "/layouts/_navigation.erb"
 		response.should have_tag('a[href=?]', home_path, 'Home')
+		response.should have_tag('a[href=?]', about_path, 'About')
 		response.should have_tag('a[href=?]', lists_path, 'Lists')
-		response.should have_tag('a[href=?]', projects_path, 'Projects')
+		response.should have_tag('a[href=?]', stocks_path, 'Stocks')
 		response.should have_tag('a[href=?]', resume_path, 'Resume')
 		response.should have_tag('a[href=?]', contact_path, 'Contact')
-		response.should have_tag('a[href=?]', 'http://blog.adamsoltys.com', 'Blog')
 	end
 end
