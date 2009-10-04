b0a99d892e3ea6bee5e291b6040b2d901be3f559
Adam Soltys <asoltys@gmail.com>
2009-10-03 19:21:56 -0700

    Create posts archive

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,8 @@ class PostsController < ApplicationController
   before_filter :login_required, :except => ['index', 'show']
 
   def index
-    @posts = Post.find(:all)
+    @posts = Post.find(:all, :order => 'created_at DESC')
+		@posts_by_month = @posts.group_by{ |p| p.created_at.strftime('%B %Y') }
   end
 
   def show
