96196e59bd6555d761fc04bf4341a92896ac93e4
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2010-12-12 13:13:03 -0700

    removed static index page, fixed routes to use the :to key

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-AdamSoltys::Application.routes do
+AdamSoltys::Application.routes.draw do
 
 	# Root Page
 
@@ -6,14 +6,14 @@ AdamSoltys::Application.routes do
 
 	# Static Pages
 
-	match '/home', 'application#home'
-	match '/archive', 'application#archive'
-	match '/about', 'application#about'
-	match '/lists', 'application#lists'
-	match '/contact', 'application#contact'
-	match '/resume', 'application#resume'
-	match '/finances', 'finances#index'
-  match '/blog', 'application#home', :defaults => { :format => 'atom' }
+	match '/home', :to => 'application#home'
+	match '/archive', :to => 'application#archive'
+	match '/about', :to => 'application#about'
+	match '/lists', :to => 'application#lists'
+	match '/contact', :to => 'application#contact'
+	match '/resume', :to => 'application#resume'
+	match '/finances', :to => 'finances#index'
+  match '/blog', :to => 'application#home', :defaults => { :format => 'atom' }
 	
 	# Resources
 	
@@ -24,22 +24,23 @@ AdamSoltys::Application.routes do
 	resources :projects
 	resources :reviews
 	resources :review_categories
-  resource :session
 	resources :transactions
 	resources :users
 
+  resource :session
+
 	# User Routes
 	
-  match '/logout', 'sessions#destroy'
-  match '/login', 'sessions#new'
-  match '/register', 'users#create'
+  match '/logout', :to => 'sessions#destroy'
+  match '/login', :to => 'sessions#new'
+  match '/register', :to => 'users#create'
 	
 	# Finance Routes
 	
-	match '/stocks', 'finances#stocks'
-	match '/update_stocks', 'finances#update_stocks'
-	match '/expire_cache', 'application#expire_cache'
-	match '/quotes/:symbol', 'quotes#new'
+	match '/stocks', :to => 'finances#stocks'
+	match '/update_stocks', :to => 'finances#update_stocks'
+	match '/expire_cache', :to => 'application#expire_cache'
+	match '/quotes/:symbol', :to => 'quotes#new'
 	
   # Default Routes
 
