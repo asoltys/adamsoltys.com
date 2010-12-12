db930cd78911255d532b39caeacb7984d43afa88
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2010-12-12 01:27:09 -0700

    converting to new active record syntax

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_filter :login_required, :except => ['index', 'show']
 
   def index
-    @posts = Post.find(:all, :order => 'created_at DESC')
+    @posts = Post.order('created_at DESC')
 		@posts_by_month = @posts.group_by{ |p| p.created_at.strftime('%B %Y') }
   end
 
