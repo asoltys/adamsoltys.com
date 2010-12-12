d2fad868533d1118c7c9356a2b19918a0d1c887b
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2010-12-12 02:12:12 -0700

    ran 'rails' command to get/upgrade to all the latest rails skeleton components

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'a5d3fcf7a28dcc5a3500820ebf08091ba7f6b962843bd3f284d7282fbd7eb41249ae2b30b1fd40d280c433caecfa75a94daabe38f61f197b96b5d8b3a23bdb04';
