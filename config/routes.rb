eafee0cf72bb2e9cdd109e0dafb7a82f5537d7d8
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2010-12-14 02:06:14 -0700

    cleaned up routes, use devise before_filter for restricted actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 AdamSoltys::Application.routes.draw do
-
-  devise_for :users
-
-	# Root Page
-
 	root :to => 'application#home'
-
-	# Static Pages
 
 	match '/home', :to => 'application#home'
 	match '/archive', :to => 'application#archive'
@@ -17,36 +10,18 @@ AdamSoltys::Application.routes.draw do
 	match '/finances', :to => 'finances#index'
   match '/blog', :to => 'application#home', :defaults => { :format => 'atom' }
 	
-	# Resources
-	
 	resources :accounts
 	resources :companies
 	resources :executions
 	resources :posts
-	resources :projects
-	resources :reviews
-	resources :review_categories
-	resources :transactions
-	resources :users
 
-  resource :session
-
-	# User Routes
-	
-  match '/logout', :to => 'sessions#destroy'
-  match '/login', :to => 'sessions#new'
-  match '/register', :to => 'users#create'
-	
-	# Finance Routes
+  devise_for :users
 	
 	match '/stocks', :to => 'finances#stocks'
 	match '/update_stocks', :to => 'finances#update_stocks'
 	match '/expire_cache', :to => 'application#expire_cache'
 	match '/quotes/:symbol', :to => 'quotes#new'
 	
-  # Default Routes
-
   match ':controller/:action/:id'
   match ':controller/:action/:id.:format'
-
 end
